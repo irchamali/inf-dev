350d7a20c79ea0b1fe7574f5593f2716fe349f20
irchamali <id.irchamali@gmail.com>
2024-05-12 20:09:31 +0700

    Update view of post category

--- app/Views/post_category.php
+++ app/Views/post_category.php
@@ -1,56 +1,36 @@
-<?= $this->extend('layout/template-home'); ?>
+<?= $this->extend('layout/template-postx'); ?>
 <?= $this->section('content'); ?>
-
-<main id="main">
-
     <!-- ======= Breadcrumbs ======= -->
-    <?= $this->include('layout/breadcrumbs'); ?>
+    <?= $this->include('layout/breadcrumbsx'); ?>
     <!-- End Breadcrumbs -->
-
-    <!-- ======= Blog Section ======= -->
-    <section id="blog" class="blog">
-        <div class="container" data-aos="fade-up">
-
-            <header class="section-header">
-                <h2 class="text-center"><?= $keyword; ?></h2>
-            </header><br>
-
-            <div class="row gy-4 posts-list">
+        <section class="module">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-serif"><?= $keyword; ?></h2>
+              </div>
+            </div>
+            <div class="row multi-columns-row post-columns">
                 <?php foreach ($posts as $post) : ?>
-                <div class="col-xl-4 col-md-6">
-                    <article>
-
-                        <div class="post-img">
-                            <img src="/assets/backend/images/post/<?= $post['post_image']; ?>" alt="<?= $post['post_title']; ?>" class="img-fluid">
+                <div class="col-sm-6 col-md-4 col-lg-4">
+                    <div class="post">
+                    <div class="post-thumbnail"><a href="/post/<?= $post['post_slug']; ?>"><img src="/assets/backend/images/post/<?= $post['post_image']; ?>" alt="Blog-post Thumbnail"/></a></div>
+                    <div class="post-header font-alt">
+                        <h2 class="post-title"><a href="/post/<?= $post['post_slug']; ?>"><?= $post['post_title']; ?></a></h2>
+                        <div class="post-meta">By&nbsp;<a href="#"><?= $post['user_name']; ?></a>&nbsp;| <time datetime="2022-01-01"><?= date('d M Y', strtotime($post['post_date'])); ?></time> | <?= $post['post_views']; ?> views
                         </div>
-
-                        <p class="post-category"><?= $post['category_name']; ?></p>
-
-                        <h2 class="title">
-                            <a href="/post/<?= $post['post_slug']; ?>"><?= $post['post_title']; ?></a>
-                        </h2>
-
-                        <div class="d-flex align-items-center">
-                            <img src="/assets/backend/images/users/<?= $post['user_photo']; ?>" alt="<?= $post['post_title']; ?>"
-                                class="img-fluid post-author-img flex-shrink-0">
-                            <div class="post-meta">
-                                <p class="post-author-list"><?= $post['user_name']; ?></p>
-                                <p class="post-date">
-                                    <time datetime="2022-01-01"><?= date('d M Y', strtotime($post['post_date'])); ?></time>
-                                </p>
-                            </div>
-                        </div>
-
-                    </article>
-                </div><!-- End post list item -->
+                    </div>
+                    <div class="post-entry">
+                    </div>
+                    <div class="post-more"><a class="more-link" href="/post/<?= $post['post_slug']; ?>">Read more</a></div>
+                    </div>
+                </div>
                 <?php endforeach; ?>
-            </div><!-- End blog posts list -->
-            
-            <!-- End blog pagination -->
-
-        </div>
-    </section><!-- End Blog Section -->
-
-</main><!-- End #main -->
+            </div>
+            <div class="pagination font-alt">
+                <!-- <a href="#"><i class="fa fa-angle-left"></i></a><a class="active" href="#">1</a><a href="#">2</a><a href="#">3</a><a href="#">4</a><a href="#"><i class="fa fa-angle-right"></i></a> -->
+            </div>
+          </div>
+        </section>
 
 <?= $this->endSection(); ?>
