f08ff670ed9a2c85540123c0d67fb1a2f181cac3
irchamali <id.irchamali@gmail.com>
2024-05-14 20:38:49 +0700

    Update favicons on header

--- app/Views/layout/header-postx.php
+++ app/Views/layout/header-postx.php
@@ -7,22 +7,15 @@
     <!--  Document Title ============================================= -->
     <title><?= $title; ?> | Teknik Informatika Unusia</title>
     <!--  Favicons ============================================= -->
-    <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(''); ?>assets/titan/images/favicons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url(''); ?>assets/titan/images/favicons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(''); ?>assets/titan/images/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url(''); ?>assets/titan/images/favicons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(''); ?>assets/titan/images/favicons/favicon-16x16.png">
-    <!-- <link rel="manifest" href="/manifest.json"> -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url(''); ?>assets/titan/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="<?= base_url(''); ?>assets/titan/images/favicon/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="<?= base_url(''); ?>assets/titan/images/favicon/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= base_url(''); ?>assets/titan/images/favicon/favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url(''); ?>assets/titan/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url(''); ?>assets/titan/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url(''); ?>assets/titan/images/favicon/site.webmanifest">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="<?= base_url(''); ?>assets/titan/images/favicons/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="<?= base_url(''); ?>assets/titan/images/favicon/mstile-150x150.png">
     <meta name="theme-color" content="#ffffff">
     <!--  Stylesheets ============================================= -->
     <!-- Default stylesheets-->
